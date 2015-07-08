5b7ccb457b11a6502852adf11c292ed9a841dfd8
fanfeilong <fanfeilong@xunlei.com>
2015-07-09 01:32:14 +0800

    add excel lib exceltk support office 97-2003 remove ExcelGUI's bin and obj directories

---
+++ src/commandline/excel/Core/XlsCell.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excel.Core
+{
+    public class XlsCell
+    {
+        private object m_object;
+        private bool m_isHyperLink;
+        private string m_hyperLink;
+
+        public XlsCell(object obj)
+        {
+            m_object = obj;
+        }
+        public void SetHyperLink(string url)
+        {
+            m_isHyperLink = true;
+            m_hyperLink = url;
+        }
+
+        public object Value
+        {
+            get { return m_object; }
+        }
+
+        public bool IsHyperLink
+        {
+            get{ return m_isHyperLink;}
+        }
+
+        public string HyperLink
+        {
+            get { return m_hyperLink; }
+        }
+
+        public string MarkDownText
+        {
+            get { 
+                return  IsHyperLink
+                    ? string.Format("[{0}]({1})",m_object,m_hyperLink)
+                    : m_object.ToString();
+            }
+        }
+
+        public override string ToString()
+        {
+            return m_object.ToString();
+        }
+    }
+}
